4116955e74dedfb045a0a273f9268e0010014059
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-08-29 13:58:27 -0600

    completes room from category functionality in iteration 3

---
+++ lib/house.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class House
+
+  attr_reader :address, :price_string, :rooms
+
+  def initialize(price_string, address_string)
+    @price_string = price_string
+    @address = address_string
+    @rooms = []
+  end
+
+  def price
+    price_array = @price_string.chars
+    price_array.delete_if do |price_char|
+      price_char == "$"
+    end
+    price_array.join("").to_i
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+  def rooms_from_category(category)
+    @rooms.keep_if do |room|
+      room.category == category
+    end
+  end
+
+end
---
+++ test/house_test.rb
@@ -0,0 +1,60 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+
+class HouseTest < Minitest::Test
+
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+    assert_instance_of House, house
+  end
+
+  def test_it_has_price_and_address_attributes
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "$400000", house.price_string
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_it_converts_price_string_to_integer
+    house = House.new("$400000", "123 sugar lane")
+    expected = 400000
+    assert_equal expected, house.price
+  end
+
+  def test_you_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    assert_equal [room_1], house.add_room(room_1)
+    assert_equal [room_1, room_2], house.add_room(room_2)
+  end
+
+  def test_you_can_call_rooms_from_a_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_you_can_call_rooms_from_a_different_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+  end
+
+end
